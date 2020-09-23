ec226655e5254c4a07c49fd8ec86af0d94410707
Keshiga Gopalarajah <z5316004@unsw.edu.au>
2020-09-24 00:23:39 +1000

    Created file channel_test.py and added a test for channel_invite

---
+++ src/channel_test.py
@@ -0,0 +1,19 @@
+# test file for the channel functions
+
+from auth import auth_register
+from channel import channel_invite, channel_details, channel_messages
+from channels import channels_create
+import pytest
+from error import InputError
+
+# check channel_invite
+# This test is meant to check if an inputError is given if the channel_id is 
+# invalid
+def test_channel_invite_except:
+    result = auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    randChannel_id = channels_create(result['token'], 'randChannel', True)
+    invalidChannel_id = 18
+    assert invalidChannel_id != randChannel_id
+    with pytest.raises(InputError) as e:
+        assert channel_invite(result['token'], randomChannel_id, result['u_id'])
+    
