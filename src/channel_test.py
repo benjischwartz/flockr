f7eb1b269efc208bd6d10ab133647f2c9fcd7632
Keshiga Gopalarajah <z5316004@unsw.edu.au>
2020-09-24 18:10:01 +1000

    Commenting out test

--- src/channel_test.py
+++ src/channel_test.py
@@ -6,11 +6,16 @@ from channels import channels_create
 import pytest
 from error import InputError
 
+
+
+
+'''
 # check channel_invite
 # This test is meant to check if an inputError is given if the channel_id is 
 # invalid; INCOMPLETE
 # Note: "result['token'] may not be valid python but my intention was to get
 # the token and u_id values that were returned from auth_register 
+
 def test_channel_invite_except:
     result = auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     randChannel_id = channels_create(result['token'], 'randChannel', True)
@@ -24,4 +29,7 @@ def test_channel_invite_except:
     # Accesserror when token is invalid 
     
     # Accesserror when the authorised user is not already a member of the channel
+'''
+    
+        
     
