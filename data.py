4c0fea04edfaee55ee088a2b10aad29673182e53
trollic <cece.jzhu@gmail.com>
2020-09-24 19:41:30 +1000

    add data structure with separate channel dict

--- data.py
+++ data.py
@@ -1,26 +1,42 @@
-# example global variable(s) for milestone 1 data
-data = {
-    'users' : {
-        'email' : {
-            'name_first' : " ",
-            'name_last' : " ",
-            'u_id' : "0000" ,
-        }
-    },
-    'channels' : {
-        'u_id' : "name"
-    },
-    'messages' : {
-        'channel' : ['history'],
+
+users = {
+    'email': {
+      'name_first' : " ",
+      'name_last' : " ",
+      'u_id' : "0023423"
     }
 }
 
-# example register function
-# login can look up dict using email as key for quick
-def register(email, password, name_first, name_last):
-    userInfo = dict()
-    userInfo['password'] = password
-    userInfo['name_first'] = name_first
-    userInfo['name_last'] = name_last
-    # TODO generate and add unique id
-    data['users'][email] = userInfo
+
+channel = {
+    'channel_id(int)' : {
+      'channel_name(str)' : " ",
+      'owner_members(str)': {
+        78978 : True,
+        223423 : True,
+        567 : True
+      },
+      'all_members(str)' : {
+        78978 : True,
+        223423 : True,
+        567 : True
+      },
+      'messages(str)' : {
+        'message(int)' : {
+          'u_id(int)' : " ",
+          'message_content' : " ",
+          'time' : " "
+        }
+      }
+  
+}      
+ 
+
+######### channels dict: matches channel_id (int) to channel name (string)
+# i.e. key is the channel id integer and value is name of channel
+# starter channels: one named "cat", other named "dog"
+channels = {
+    1 : "cat",
+    2 : "dog",
+    'totalChannels' : 2,
+}
