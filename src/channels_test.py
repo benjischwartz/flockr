8e91af8e24771fb7f93d30a5ee385c771f1f6ebc
trollic <cece.jzhu@gmail.com>
2020-09-24 17:16:39 +1000

    added addtional channels_tests: user only view for channels_list and private/public

--- src/channels_test.py
+++ src/channels_test.py
@@ -14,16 +14,18 @@ def test_return_type():
     assert(type(channels_listall("randtoken")) is dict)
     assert(type(channels_create("randtoken", "somename", True)) is dict)
 
-# check if user can view only appropriate lists they have joined
+# TODO: check if user can view only appropriate lists they have joined
 def test_channels_list_user_view():
     # create user and compare channels_list result with channels_list_all
-    assert True
+    assert False, "not implemented: channels_list restict only to users within each channel"
 
 
 # check channels_create adds to the total channel
 ##### pre-implement should raise exception
-def test_channels_create_preimplement():
-    with pytest.raises(Exception, match=r"Error, channel create does not actually add a new channel to total"):
-        channels_create("something", "something", True)
+def test_channels_create_too_long_name():
+    with pytest.raises(InputError, match=r"channel name cannot be greater than 20 characters"):
+        channels_create("something", "a_string_name_which_is_very_long_and_will_never_pass", True)
     
-    #TODO: check type of channel: public/private is valid
+#TODO: check type of channel: public/private is value
+def test_channels_create_private_public():
+    assert False, "not implemented: distinction between public and private channels for channels_create"
