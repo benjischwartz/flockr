eebfc03f8da1961471d583e806afb5ec6c6059ef
trollic <cece.jzhu@gmail.com>
2020-09-24 10:16:09 +1000

    added testing exceptions (temp) to channels.py

--- src/channels.py
+++ src/channels.py
@@ -20,6 +20,13 @@ def channels_listall(token):
     }
 
 def channels_create(token, name, is_public):
+    ## start testing exceptions
+    numChannelsbefore = len(channels_listall("randtoken"))
+    ## do something to actually create channel
+    numChannelsafter = len(channels_listall("randtoken"))
+    if (numChannelsbefore != numChannelsafter - 1):
+        raise Exception(f"Error, channel create does not actually add a new channel to total")
+    ## end testing exceptions
     return {
         'channel_id': 1,
     }
--- src/channels_test.py
+++ src/channels_test.py
@@ -14,23 +14,16 @@ def test_return_type():
     assert type(channels_listall("randtoken")) is dict
     assert type(channels_create("randtoken", "somename", True)) is dict
 
-# check channels_create adds to the total channels
-def test_channels_create():
-    numChannelsbefore = len(channels_listall("randtoken"))
-    channels_create("randtoken", "somename", True)
-    numChannelsafter = len(channels_listall("randtoken"))
-    if (numChannelsbefore != numChannelsafter - 1):
-        raise Exception(f"Error, channel create does not actually add a new channel to total")
-    #TODO: test public/private toggle
-
 # check if user can view only appropriate lists they have joined
 def test_channels_list_user_view():
     # create user and compare channels_list result with channels_list_all
     assert True
 
 
-##### try catch exceptions test
-try:
-    test_channels_create()
-except Exception:
-    print("Create new Channel function not working/implemented")
+# check channels_create adds to the total channel
+##### pre-implement should raise exception
+def test_channels_create_preimplement():
+    with pytest.raises(Exception, match=r"Error, channel create does not actually add a new channel to total"):
+        channels_create("something", "something", True)
+    
+    #TODO: check type of channel: public/private is valid
