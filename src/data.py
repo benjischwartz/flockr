de94af806267f5506f59b9502364ffef1c5ba1a5
Benjamin Schwartz <benji.schwartz2013@gmail.com>
2020-09-26 08:30:58 +1000

    updated data.py file to current version

--- src/data.py
+++ src/data.py
@@ -1,27 +1,32 @@
-# this is a redundant comment
-# example global variable(s) for milestone 1 data
-data = {
-    'users' : {
-        'email' : {
-            'name_first' : " ",
-            'name_last' : " ",
-            'u_id' : "0000" ,
-        }
-    },
-    'channels' : {
-        'u_id' : "name"
-    },
-    'messages' : {
-        'channel' : ['history'],
+
+users = {
+    'u_id(int)': {
+    'name_first' : " ",
+    'name_last' : " ",
+    'email' : " "
     }
 }
 
-# example register function
-# login can look up dict using email as key for quick
-def register(email, password, name_first, name_last):
-    userInfo = dict()
-    userInfo['password'] = password
-    userInfo['name_first'] = name_first
-    userInfo['name_last'] = name_last
-    # TODO generate and add unique id
-    data['users'][email] = userInfo
+
+channel = {
+    'channel_id(int)' : {
+        'channel_name(str)' : " ",
+        'owner_members(str)': {
+            78978 : True,
+            223423 : True,
+            567 : True
+        },
+        'all_members(str)' : {
+            78978 : True,
+            223423 : True,
+            567 : True
+        },
+        'messages(str)' : {
+            'message(int)' : {
+              'u_id(int)' : " ",
+              'message_content' : " ",
+              'time' : " "
+            }
+        }
+    }
+
