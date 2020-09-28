9e100edcc5d981bf057c0c26a3e9132d04d96fc0
Benjamin Schwartz <benji.schwartz2013@gmail.com>
2020-09-28 11:48:10 +1000

    fixed syntax

--- src/data.py
+++ src/data.py
@@ -15,8 +15,8 @@ tokens = {
 channel = {
     'channel_id(int)' : {
         'channel_name(str)' : " ",
-        'is_public' : False
-        'owner_members(str)': {
+        'is_public' : False,
+        'owner_members(str)' : {
             78978 : True,
             223423 : True,
             567 : True
