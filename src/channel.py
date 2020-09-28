9c1a6b7ed86ce873b7b5fd321348dba58b6222a4
z5312704 <z5312704@ad.unsw.edu.au>
2020-09-29 09:34:28 +1000

    Version 6 Debuggin

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,7 @@
 from auth import auth_login, auth_logout, auth_register
 from data import users, tokens, channel
 from error import InputError, AccessError
+from check_token import is_valid_token, user_id_given_token
 
 
 def channel_invite(token, channel_id, u_id):
@@ -130,16 +131,16 @@ def channel_addowner(token, channel_id, u_id):
     if channel_id not in channel:
         #If Channel ID is invalid
         raise InputError("Channel ID is invalid")
-    if u_id in channel['owner_members'].keys():
+    if u_id in channel[channel_id]['owner_members']:
         #If User ID is already an owner of the channel
         raise InputError("User is already an owner")
     
     #If current token is not an owner of the channel
-    if channel['owner_members'].has_key(token) == False:
+    if user_id_given_token(token) not in channel[channel_id]['owner_members']:
         raise AccessError("You are not an owner")
 
     #ADDING THE USER TO THE LIST OF OWNERS
-    channel['owner_members'][u_id] = True
+    channel[channel_id]['owner_members'][u_id] = True
     return {
 
     }
@@ -150,20 +151,20 @@ def channel_removeowner(token, channel_id, u_id):
     if channel_id not in channel:
         #If Channel ID is invalid
         raise InputError("Channel ID is invalid")
-    if u_id in channel['owner_members'].keys():
+    if u_id in channel[channel_id]['owner_members']:
         #If User ID is already an owner of the channel
         raise InputError("User is already an owner")
     
     #If current token is not an owner of the channel
-    if channel['owner_members'].has_key(token) == False:
+    if user_id_given_token(token) not in channel[channel_id]['owner_members']:
         raise AccessError("You are not an owner")
 
     #If there are no other owners (ASSUMPTION)
-    if (len(channel['owner_members']) <= 1):
+    if (len(channel[channel_id]['owner_members']) <= 1):
         raise Exception("There must be at least one other owner in order to remove an owner")
 
     #REMOVING OWNER FROM THE LIST OF USERS
-    channel['owner_members'].pop(u_id)
+    channel[channel_id]['owner_members'].pop(u_id)
 
     return {
 
