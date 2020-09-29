957910b00e6ce948078e783c5027d4f7bf63cdeb
z5312704 <z5312704@ad.unsw.edu.au>
2020-09-29 16:02:01 +1000

    Version 8 Debugging

--- src/channel.py
+++ src/channel.py
@@ -135,6 +135,7 @@ def channel_addowner(token, channel_id, u_id):
         #If User ID is already an owner of the channel
         raise InputError("User is already an owner")
     
+    
     #If current token is not an owner of the channel
     if user_id_given_token(token) not in channel[channel_id]['owner_members']:
         raise AccessError("You are not an owner")
