b4e8fdc5154115ec0b18dbddecfe4e1a3f450a24
trollic <cece.jzhu@gmail.com>
2020-10-01 09:51:08 +1000

    added comments to data.py

--- src/data.py
+++ src/data.py
@@ -1,5 +1,5 @@
 
-users = {
+users = { # 'email' is the actual emails of users
     'email': {
         'u_id' : " ",
         'name_first' : " ",
@@ -10,16 +10,16 @@ users = {
 
 tokens = ['benji.schwartz@gmail.com']
 
-channel = {
+channel = { # 'channel_id' are the int values themselves, per channel
     'channel_id' : {
         'channel_name' : " ",
         'is_public' : False,
-        'owner_members': {
+        'owner_members': { #numbers in owner members are the user ids
             78978 : True,
             223423 : True,
             567 : True
         },
-        'all_members' : {
+        'all_members' : { #numbers in all members are the user ids
             78978 : True,
             223423 : True,
             567 : True
