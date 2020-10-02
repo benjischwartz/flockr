f002bbd5e9a50194535e92931fde6d0b66d11c9f
z5312704 <z5312704@ad.unsw.edu.au>
2020-10-03 09:28:45 +1000

    Version 14 Error Checking

--- src/channel_test.py
+++ src/channel_test.py
@@ -474,5 +474,15 @@ def test_channel_removeowner_not_owner_permissions():
     
 #checking if owner of the flockr who is not the channel owner can remove owner 
 def test_channel_removeowner_owner_flockr():
-    pass
+    clear()
+    #Registering First User
+    registerFirst_result = auth_register('randemail@gmail.com', 'password1234', 'Jane', 'Citizen')
+    #Creating Channel
+    randChannel_id = channels_create(registerFirst_result['token'], 'Random Channel', True)
+    #Registering Second User
+    registerSecond_result = auth_register('randemail2@gmail.com', 'password1234', 'Jane', 'Citizen')
+    #Making Second User an Owner of Channel
+    channel_addowner(registerFirst_result['token'], randChannel_id['channel_id'], registerSecond_result['u_id'])
+    #Removing Second User
+    channel_removeowner(registerFirst_result['token'], randChannel_id['channel_id'], registerSecond_result['u_id'])
 
