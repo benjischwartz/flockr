f414bc1721e7b1cf5136123a3917fb119841eda8
trollic <cece.jzhu@gmail.com>
2020-10-02 16:51:03 +1000

    removed check tokens, put functions in channels.py instead

--- src/channels.py
+++ src/channels.py
@@ -1,6 +1,4 @@
 from data import channel, users
-from channel import channel_details
-from check_token import is_valid_token, user_id_given_token
 from error import InputError, AccessError
 
 
@@ -107,3 +105,21 @@ def channels_create(token, name, is_public):
     # return {
     #     'channel_id': newChannel_id
     # }
+
+def is_valid_token(token):
+    '''
+    returns true or false if token, email, is in user database
+    '''
+    if token in users:
+        return True
+    return False
+
+def user_id_given_token(token):
+    '''
+    returns the u_id if the user is in the database
+    otherwise returns None if u_id is not found
+    '''
+    if token in users:
+        user_id = users[token]['u_id']
+        return user_id
+    return None
