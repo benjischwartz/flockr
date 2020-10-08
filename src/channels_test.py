0c833d27e48e1c570078f525f49412db40abe965
trollic <cece.jzhu@gmail.com>
2020-10-08 15:38:14 +1100

    100% pylint channels and channels_test

--- src/channels_test.py
+++ src/channels_test.py
@@ -13,9 +13,9 @@ from other import clear
 def test_return_type():
     # clear database and register new dummy users for testing
     clear()
-    userOne = auth_register("first@example.com", "password1234", " ", " ")
-    userTwo = auth_register("second@example.com", "password1234", " ", " ")
-    userThree = auth_register("third@example.com", "password1234", " ", " ")
+    auth_register("first@example.com", "password1234", " ", " ")
+    auth_register("second@example.com", "password1234", " ", " ")
+    auth_register("third@example.com", "password1234", " ", " ")
     # check types
     assert(type(channels_list("first@example.com")) is dict) 
     assert(type(channels_listall("second@example.com")) is dict)
@@ -73,8 +73,8 @@ def test_channels_list_user_view():
     userOne = auth_register("first@example.com", "password1234", " ", " ")
     userTwo = auth_register("second@example.com", "password1234", " ", " ")
     # create channels that has no users
-    emptyChannel = channels_create(userOne['token'], "validchannel1", True)
-    userChannel = channels_create(userTwo['token'], "validchannel2", True)
+    channels_create(userOne['token'], "validchannel1", True)
+    channels_create(userTwo['token'], "validchannel2", True)
     # create user and compare channels_list result with channels_list_all
     assert channels_list(userOne['token']) != channels_listall(userTwo['token']), "users can see all lists, even if not a member"
     assert len(channels_list(userOne['token'])['channels']) == 1, "channel list per user sees unexpected number"
