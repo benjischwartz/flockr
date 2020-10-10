4758c60f9bd9286cfbfa11b7ec864ced7e3de82b
z5312704 <z5312704@ad.unsw.edu.au>
2020-10-10 12:56:51 +1100

    Adding Comments User Test

--- src/user_test.py
+++ src/user_test.py
@@ -2,12 +2,40 @@ import user
 import pytest
 
 #TODO: ETHAN 
-def test_user_setname():
+#User Setname Tests
+def test_user_setname_positive_case():
     pass
 
-def test_user_setemail():
+def test_user_setname_name_first_short():
+    pass
+
+def test_user_setname_name_first_long():
+    pass
+
+def test_user_setname_name_last_short():
+    pass
+
+def test_user_setname_name_last_long():
+    pass
+
+# User Setemail Tests
+def test_user_setemail_positive_case():
+    pass
+
+def test_user_setemail_already_used():
+    pass
+
+def test_user_setemail_invalid_email_no_at_symbol():
+    pass
+
+def test_user_setemail_not_alphanumeric():
     pass
 
 #TODO: BRIAN
+# User Profile Tests
+def test_user_profile():
+    pass
+
+# User Sethandle Tests
 def test_user_sethandle():
     pass
