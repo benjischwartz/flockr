c7fc4bfce5b0b66c0e677f6e59095254e5d00a0f
trollic <cece.jzhu@gmail.com>
2020-10-10 11:28:43 +1100

    reduced check_token branching

--- src/check_token.py
+++ src/check_token.py
@@ -6,9 +6,8 @@ def user_id_given_token(token):
     if the user is in database AND is logged in
     otherwise returns None
     '''
-    if token in tokens:
-        if token in users:
-            user_id = users[token]['u_id']
-            return user_id
-    return None
+    if token in tokens and token in users:
+        return users[token]['u_id']
+    else:        
+        return None
 
