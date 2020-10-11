19c86d4df113bc844034dee5a01b5451083e7941
trollic <cece.jzhu@gmail.com>
2020-10-11 15:16:26 +1100

    added search access error, updated requirements.txt to include all https://www.cse.unsw.edu.au/~cs1531/20T3/requirements.txt

--- src/search.py
+++ src/search.py
@@ -1,5 +1,10 @@
+from check_token import user_id_given_token
+from error import AccessError
+
 def search(token, query_string):
     """
     Given a query string, return a collection of messages in all of the channels that the user has joined that match the query
     Return {messages}
     """
+    if user_id_given_token(token) == None:
+        raise(AccessError) 
