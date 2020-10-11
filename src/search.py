6039264b147c97c34e2c342a3b62c7a3dcccfcf7
trollic <cece.jzhu@gmail.com>
2020-10-11 15:07:47 +1100

    added search() docstring and added to requirements.txt based on starter code modules

---
+++ src/search.py
@@ -0,0 +1,5 @@
+def search(token, query_string):
+    """
+    Given a query string, return a collection of messages in all of the channels that the user has joined that match the query
+    Return {messages}
+    """
