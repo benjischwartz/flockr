492977a2c83ec433cf63e411f00e73beb56875cb
Cece <cece.jzhu@gmail.com>
2020-10-12 15:54:47 +1100

    outline permissions test cases and added docstring

--- src/admin_permissions_change_test.py
+++ src/admin_permissions_change_test.py
@@ -1,1 +1,13 @@
 import pytest
+
+# test change from owner of flocker to non-owner
+
+# test change from non-owner of flocker to owner
+
+# test access error
+
+# test import error
+
+# test permissions id
+
+# test u_id
