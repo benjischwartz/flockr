f9fbca754cf7ef6efd9836eecb04a9a0cae537d7
Cece <cece.jzhu@gmail.com>
2020-10-12 15:54:22 +1100

    outline permissions test cases and added docstring

---
+++ src/admin_permissions_change.py
@@ -0,0 +1,8 @@
+from data import users
+
+# required function changing permission
+def change_permissions(token,u_id,permission_id):
+    """
+    Given a User by their user ID, set their permissions
+     to new permissions described by permission_id
+    """
