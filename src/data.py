56719d133ae925eb3baf771081e4ad2aad49581f
Zeyang <z5296414@ad.unsw.edu.au>
2020-10-14 09:10:24 +1100

    added handle to data.py

--- src/data.py
+++ src/data.py
@@ -3,10 +3,10 @@
 users = {
     'email': {
         'u_id' : " ",
-        'handle_str': " ",
         'name_first' : " ",
         'name_last' : " ",
         'password' : " ",
+        'handle' : " "
         }
     }
 
