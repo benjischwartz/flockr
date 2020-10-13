8ac63f7bdde26d549368f71f2b82dd0d3b157ec5
trollic <cece.jzhu@gmail.com>
2020-10-13 18:11:49 +1100

    added single message, single channel test

--- src/search_test.py
+++ src/search_test.py
@@ -4,6 +4,7 @@ import error
 import pytest
 from channels import channels_create, channels_list
 from auth import auth_register
+from message import message_send
 
 # test for Access Error and Invalid Token
 def test_access_error():
@@ -14,10 +15,14 @@ def test_access_error():
 # test for positive case, single message, single channel
 def test_single_channel_single_message():
     clear()
-    # TODO - register user and create channel
+    # register user and create public channel
+    user_one = auth_register("first@user.com", "password", "Jane", "Applebaum")
+    new_channel = channels_create(user_one['token'],"channel_one", True)
     # create a single message in the channel
-    # TODO - call channels_list to get list of every channel user is part of
-    # TODO - Loop through every element each channel
+    message_send(user_one['token'], new_channel['channel_id'], "this is a message")
+    # check one message is matched
+    assert len(search(user_one['token'], "this is")) == 1, "does not return expected number of messages"
+    assert len(search(user_one['token'], "this is a message")) == 1, "does not return expected number of messages"
 
 # test where search query matches in channel user NOT part of
 # expect this message is not in the return of search()
