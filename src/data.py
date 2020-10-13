dc1b15154059d599536edd03229068c5e911e031
Zeyang <z5296414@ad.unsw.edu.au>
2020-10-13 11:16:41 +1100

    user_data added handle_str

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@
 users = {
     'email': {
         'u_id' : " ",
-        'handle': " ",
+        'handle_str': " ",
         'name_first' : " ",
         'name_last' : " ",
         'password' : " ",
