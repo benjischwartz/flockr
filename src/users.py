bfc15c597aaa2a94b6f63c8b091afbea46430925
Zeyang <z5296414@ad.unsw.edu.au>
2020-10-14 20:12:03 +1100

    added positive test for users/all

--- src/users.py
+++ src/users.py
@@ -1,5 +1,6 @@
 from data import users, channel
 from check_token import user_id_given_token
+from user import user_profile
 
 def users_all(token):
     pass
---
+++ src/users_test.py
@@ -0,0 +1,29 @@
+import pytest
+from auth import auth_register
+from user import user_sethandle
+from users import users_all
+from other import clear
+
+def test_users_all_positive_test():
+    clear()
+    userOne = auth_register('firstuser@gmail.com', '123abc!@#', 'First', 'User')
+    user_sethandle(userOne['token'], '12345')
+    userTwo = auth_register('seconduser@gmail.com', '456abc!@#', 'Second', 'User')
+    user_sethandle(userTwo['token'], '123456')
+    details = users_all(userOne['token'])
+    assert(details == [
+    {
+        'u_id' : userOne['u_id'],
+        'name_first' : 'First',
+        'name_last' : 'User',
+        'handle' : '12345',
+        'email' : 'firstuser@gmail.com'
+    },
+    {
+        'u_id' : userTwo['u_id'],
+        'name_first' : 'Second',
+        'name_last' : 'User',
+        'handle' : '123456',
+        'email' : 'seconduser@gmail.com'
+    }
+    ])
