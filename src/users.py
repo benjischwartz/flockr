81d36a897f0754a559c13618b22a9eaa472c6f86
Zeyang <z5296414@ad.unsw.edu.au>
2020-10-14 21:18:42 +1100

    Completed users_all function version 1

--- src/users.py
+++ src/users.py
@@ -1,6 +1,15 @@
 from data import users, channel
 from check_token import user_id_given_token
 from user import user_profile
+from error import InputError, AccessError
 
 def users_all(token):
-    pass
+    temp = users
+    token_u_id = user_id_given_token(token)
+    if token_u_id is None:
+        raise AccessError("Token passed is not valid.")
+    
+    for token in temp:
+        temp[token].pop('password')
+
+    return temp
