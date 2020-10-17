942dd1f69e105bacdcb4943977c8dcde2a1d0c3c
trollic <cece.jzhu@gmail.com>
2020-10-17 11:29:52 +1100

    added first test for auth_register prior to activating app route

--- src/server_test.py
+++ src/server_test.py
@@ -5,13 +5,14 @@ import signal
 from time import sleep
 import requests
 import urllib
+from other import clear
 
 
 # Use this fixture to get the URL of the server.
 @pytest.fixture
 def url():
     url_re = re.compile(r' \* Running on ([^ ]*)')
-    server = Popen(["python3", "simple.py"], stderr=PIPE, stdout=PIPE)
+    server = Popen(["python3", "src/server.py"], stderr=PIPE, stdout=PIPE)
     line = server.stderr.readline()
     local_url = url_re.match(line.decode())
     if local_url:
@@ -33,3 +34,16 @@ def test_url(url):
     A simple sanity test to check that your server is set up properly
     '''
     assert url.startswith("http")
+
+def test_auth_register(url):
+    """
+    Testing server auth_register
+    """
+    clear()
+    r = requests.post(f"{url}/auth/register/", json={
+        "email" : "first@person.com",
+        "password" : "catdog",
+        "name_first" : "Joe",
+        "name_last" : "Bloggs"})
+    # TODO: update token email after hashing done
+    assert r.json() == {"u_id" : 1, "token" : "first@person.com"}
