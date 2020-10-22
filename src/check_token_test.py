97a39d943f9c4bba3fb3193270381313e6f94421
z5312704 <z5312704@ad.unsw.edu.au>
2020-10-23 08:30:20 +1100

    Initial Add Tests

---
+++ src/check_token_test.py
@@ -0,0 +1,39 @@
+import pytest
+from check_token import user_id_given_token, get_handle, email_given_user_id, permission_id_given_token
+from auth import auth_register, auth_logout
+from other import clear
+
+def test_user_id_given_token_positive_case():
+    clear()
+    # registering first user as the first user is the owner of the flockr
+    register_first_result = auth_register('randemail@gmail.com', 'password1234', 'Jane', 'Citizen')
+    assert(user_id_given_token(register_first_result['token']) == 1) 
+
+def test_user_id_given_token_after_logout():
+    clear()
+    # registering first user as the first user is the owner of the flockr
+    auth_register('randemail@gmail.com', 'password1234', 'Jane', 'Citizen')
+    # registering second user
+    register_second_result = auth_register('secondemail@gmail.com', 'password1234', 'Second', 'User')
+    auth_logout(register_second_result['token'])
+    assert(user_id_given_token(register_second_result['token']) == None)
+
+def test_get_handle():
+    clear()
+    # registering first user 
+    register_first_result = auth_register('randemail@gmail.com', 'password1234', 'Jane', 'Citizen')
+    register_first_result_uid = user_id_given_token(register_first_result['token'])
+    assert(get_handle(register_first_result_uid) == "janecitizen")
+
+def test_email_given_user_id():
+    clear()
+    # registering first user 
+    register_first_result = auth_register('randemail@gmail.com', 'password1234', 'Jane', 'Citizen')
+    register_first_result_uid = user_id_given_token(register_first_result['token'])
+    assert(email_given_user_id(register_first_result_uid) == "randemail@gmail.com")
+
+def test_permission_id_given_token():
+    clear()
+    # registering first user 
+    register_first_result = auth_register('randemail@gmail.com', 'password1234', 'Jane', 'Citizen')
+    assert(permission_id_given_token(register_first_result['token']) == 1)
