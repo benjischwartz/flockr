41bc4a263dd2091779c9c0dc3e86a537893b177e
trollic <cece.jzhu@gmail.com>
2020-10-25 11:21:44 +1100

    added docstring for search

--- src/search.py
+++ src/search.py
@@ -5,10 +5,17 @@ from channel import channel_messages
 from data import channel
 
 def search(token, query_string):
-    """
-    Given a query string, return a collection of messages in all of the channels that the user has joined that match the query
-    Return {messages}
-    """
+    '''
+    Returns the messages in the channel the user is a part of, which 
+    contain the query string
+ 
+    Parameters:
+        token (str): refers to a valid user on flockr; this user is the inviter
+        query_string (str): the pattern string which is being searched for
+ 
+    Returns:
+        (list): {messages}
+    '''
     if user_id_given_token(token) == None:
         raise(AccessError) 
     # init results
