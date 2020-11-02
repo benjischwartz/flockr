658c66532bdcce1416721c1ae336ceba7703ad77
Benjamin Schwartz <benji.schwartz2013@gmail.com>
2020-11-02 13:51:42 +1100

    stash

--- src/auth.py
+++ src/auth.py
@@ -162,3 +162,6 @@ def auth_register(email, password, name_first, name_last):
         'u_id' : newU_id,
         'token' : encoded_jwt,
     }
+
+encoded = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1X2lkIjoiMTIzNDUifQ.lBTAPFU1xxDAi2Vrusfo67ypBai0vBr6O7KOt6CJf1s'
+jwt.decode(token, 'secret', algorithms='HS256')
